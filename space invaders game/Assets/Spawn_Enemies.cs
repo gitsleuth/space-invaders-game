e1d5161df0de11df1119d621cb1946e4b4ba22ac
gitsleuth <154627596+gitsleuth@users.noreply.github.com>
2024-06-25 18:18:15 +0100

    Make enemies move

--- space invaders game/Assets/Spawn_Enemies.cs
+++ space invaders game/Assets/Spawn_Enemies.cs
@@ -10,12 +10,18 @@ public class Spawn_Enemies : MonoBehaviour
     public int enemiesPerRow = 6;
     public int rows = 3;
     public List<GameObject> clones = new List<GameObject>();
+    public int moveInterval = 1;
 
     private int padding = 2;
     private bool spawned = false;
     private int enemies;
     private int enemiesPerRowBefore;
     private int rowsBefore;
+    private float elapsed = 0;
+    private float moveDistance = 1;
+    private float lerpElapsed = 0;
+    private float moveDuration = 0.5f;
+    private bool moving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -62,5 +68,52 @@ public class Spawn_Enemies : MonoBehaviour
                 }
             }
         }
+
+        elapsed += Time.deltaTime;
+
+        if (moving)
+        {
+            lerpElapsed += Time.deltaTime;
+        }
+        else
+        {
+            lerpElapsed = 0;
+        };
+
+        while (elapsed >= moveInterval)
+        {
+            elapsed -= moveInterval;
+
+            StartCoroutine(MoveEnemies());
+        };
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        int numClones = clones.Count;
+        Vector3[] startPositions = new Vector3[numClones];
+
+        for (int i = 0; i < numClones; i++)
+        {
+            startPositions[i] = clones[i].transform.position;
+        };
+
+        moving = true;
+
+        while (lerpElapsed < moveDuration)
+        {
+            for (int i = 0; i < numClones; i++)
+            {
+                GameObject clone = clones[i];
+
+                Transform transform = clone.gameObject.transform;
+
+                Vector3 startPosition = startPositions[i];
+
+                transform.position = Vector3.Lerp(startPosition, startPosition + Vector3.right * moveDistance, lerpElapsed / moveDuration);
+            };
+        }
+
+        moving = false;
     }
 }
