a00b26d7b777b27fdc3a93dc5a70e35859b957df
gitsleuth <154627596+gitsleuth@users.noreply.github.com>
2024-06-25 17:09:50 +0100

    Make enemies spawn on multiple rows

--- space invaders game/Assets/Spawn_Enemies.cs
+++ space invaders game/Assets/Spawn_Enemies.cs
@@ -7,25 +7,33 @@ public class Spawn_Enemies : MonoBehaviour
     public GameObject enemy;
     public GameObject spawn;
 
-    public int enemies = 4;
+    public int enemiesPerRow = 6;
+    public int rows = 3;
     public List<GameObject> clones = new List<GameObject>();
 
     private int padding = 2;
-    private int enemiesBefore;
     private bool spawned = false;
+    private int enemies;
+    private int enemiesPerRowBefore;
+    private int rowsBefore;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemiesBefore = enemies;
+        enemies = enemiesPerRow * rows;
+
+        enemiesPerRowBefore = enemiesPerRow;
+        rowsBefore = rows;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enemies != enemiesBefore)
+        if (enemiesPerRowBefore != enemiesPerRow || rowsBefore != rows)
         {
-            enemiesBefore = enemies;
+            enemiesPerRowBefore = enemiesPerRow;
+            rowsBefore = rows;
+            enemies = enemiesPerRow * rows;
             spawned = false;
 
             for (int i = clones.Count - 1; i >= 0; i--)
@@ -39,13 +47,19 @@ public class Spawn_Enemies : MonoBehaviour
         {
             spawned = true;
 
+            int row = 0;
+
             for (int i = 1; i < enemies + 1; i++)
             {
-                float offset = (i - (enemies + 1) * 0.5f) * padding;
+                float offset = ((i - row * enemiesPerRow) - (enemiesPerRow + 1) * 0.5f) * padding;
                 GameObject clone;
-                clone = Instantiate(enemy, new Vector3(spawn.transform.position.x + offset, spawn.transform.position.y), enemy.transform.rotation);
+                clone = Instantiate(enemy, new Vector3(spawn.transform.position.x + offset, spawn.transform.position.y - row * padding), enemy.transform.rotation);
                 clone.GetComponent<SpriteRenderer>().enabled = true;
                 clones.Add(clone);
+                if (i != enemies && i % enemiesPerRow == 0)
+                {
+                    row += 1;
+                }
             }
         }
     }
